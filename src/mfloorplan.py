43e74c1d33e77fcd6d5a823a99f2d906e24b3eed
Mark Riordan <riordan@rocketmail.com>
2022-04-10 07:27:40 -0700

    Add imagesize cmd.  Document cmds.
    Also add timestamp in comments.

--- src/mfloorplan.py
+++ src/mfloorplan.py
@@ -1,18 +1,80 @@
 # mfloorplan.py - program to read an input file describing a floor plan and
 # create a graphics file with a rendering of that floor plan.
-# The input file contains a list of commands.  Currently the input file is in 
-# CSV format.
+# The input file contains commands used to describe the floor plan, 
+# in CSV format. 
 # The output file is in SVG (Scaled Vector Graphics) format.
+#
+# The commands take the format:
+#   commandName,arg1,arg2,...
+# Not all commands take the same number of arguments, so this is kind of a 
+# weird CSV file.
+# Height and width in most cases are meant to be in inches, or feet and inches.
+# However, in actuality the units are arbitrary.
+#
+#-----------------------------------------------------------------------------
+# The commands are:
+# imagesize,width,height
+#   Specifies the image size.  Must be the first non-comment command.
+#   where width and height are the total size of the image in pixels.
+#
+# rect,id,name,width,height,mycorner,otherid,othercorner,relativeX,relativeY 
+#   Causes a rectangle to be drawn.
+#   id is the id of this (new) rectangle.  It can contain letters, digits,
+#     and _. Ids are case-sensitive and must start with a letter.
+#     (These rules are not yet 100% enforced yet.)
+#   name is the human-friendly name; some advanced version of the app will 
+#     draw this.  Can be empty.
+#   width is the width
+#   height is the height of the rectangle; see below for the syntax and units
+#   mycorner is the corner of this new rectangle which will be used for 
+#     relative positioning.  It's one of ll, ul, lr, ur
+#   otherid is the id of another, existing rectangle.  The special id "origin"
+#     indicates the upper left corner of the image.
+#   othercorner is the corner of that other rectangle which will be used for 
+#     relative positioning.  It is ignored for the otherid "origin". 
+#   relativeX is the relative horizontal position of the new rectangle w.r.t. 
+#     the other rectangle.  Units are as described below, with the addition 
+#     that negative numbers indicate a position to the left
+#   relativeY is the relative vertical position of the new rectangle w.r.t. 
+#     the other rectangle.  Units are as described below, with the addition 
+#     that negative numbers indicate a position above.
+#
+# class,classes
+#   Sets the CSS class(es) to be used from this point on (to the next)
+#   class command.  This applies to objects other than text.
+#   classes is a blank-separated list of CSS class names.
+#
+# textclass,classes
+#   Sets the CSS class(es) to be used from this point on (to the next)
+#   textclass command.  This applies only to text.
+#
+# repeat,id
+#   Redraws the object with the given id, using the current classes.
+#   This can be used when two objects overlap.
+#-----------------------------------------------------------------------------
+#
+#  The default unit is inches, and partial inches can be specified via both 
+# decimal places and fractions (like 5/16).   Feet and inches can also be 
+# specified.  Decimal places are allowed on both feet and inches, though 
+# they are not encouraged.
+#
+# Here are some valid sizes and their meanings:
+# 12 means 12 inches
+# 12 5/16 means 12 and 5/16 inches
+# 14' means 14 feet
+# 14' 6 means 14 feet and 6 inches
+# 14' 6 5/16 means 14 feet and 6 and 5/16 inches
 #
 # Mark Riordan  2022-03-10
 
 import argparse
 import re
+from datetime import datetime
 
 xoffset = 12
 yoffset = 12
-total_width = 1050
-total_height = 480
+total_width = 0
+total_height = 0
 current_class = ""
 current_text_class = ""
 list_classes = [current_class]
@@ -90,6 +152,11 @@ def concat_list(mylist):
     for token in mylist:
         msg += token + " "
     return msg
+
+def get_timestamp():
+    now = datetime.now()
+    current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    return current_time
 
 # Draw the text in "label" in the object given by dictIds[objid]
 def draw_label(objid,label):
@@ -221,6 +288,16 @@ def do_textclass(new_class):
         list_text_classes.append(current_text_class)
         current_text_class = new_class
 
+def do_imagesize(sizex, sizey):
+    global total_width, total_height
+    if total_width == 0:
+        total_width = sizex
+        total_height = sizey
+        write_line(f'<svg width="{total_width}" height="{total_height}" version="1.1" xmlns="http://www.w3.org/2000/svg">')
+        write_line(f"<!-- Rendered {get_timestamp()} -->")
+    else:
+        print("Error in imagesize: image size has already been set")
+
 def do_repeat(id):
     global dict_vectors
     line = dict_vectors[id]
@@ -251,6 +328,11 @@ def process_cmd(cmd, row):
             print("Bad number of args for repeat: " + concat_list(row))
         else:
             do_repeat(row[1])
+    elif cmd=="imagesize":
+        if len(row) != 3:
+            print("Bad number of args for {cmd}: " + concat_list(row))
+        else:
+            do_imagesize(row[1], row[2])
     else:
         print("Bad cmd " + cmd)
 
@@ -266,12 +348,12 @@ def write_file_header(dict_args):
     if "cssfile" in dict_args:
         cssfile = dict_args["cssfile"]
         write_line(f'<?xml-stylesheet type="text/css" href="{cssfile}"?>')
-    write_line(f'<svg width="{total_width}" height="{total_height}" version="1.1" xmlns="http://www.w3.org/2000/svg">')
 
 def write_file_footer():
     write_line("</svg>")
 
 def read_csv_file(dict_args):
+    global total_width
     infile = dict_args["infile"]
     print("About to read " + dict_args["infile"])
     with open(infile, newline='') as csvfile:
@@ -286,6 +368,8 @@ def read_csv_file(dict_args):
             cmd = row[0]
             process_cmd(cmd, row)
             pass
+    if total_width==0:
+        print("Error: no  imagesize  command found")
     write_file_footer()
     return
 
